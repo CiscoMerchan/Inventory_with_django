1747301dbea8189ffc33e7bede5506112c176993
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-04-04 13:22:46 +1000

    Commint Models

--- inventory_management/dashboard/admin.py
+++ inventory_management/dashboard/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import Supplier
+from .models import Product
 
 # Register your models here.
 
-admin.site.register(Supplier)
+# To change name of the headre at the Admin Dashboard
+admin.site.site_header ="Welcome to your Inventory"
+
+# To display in the Admin Dashboard the columns of the table as table
+"""first create a class initilase with the 'admin' module, with 'list_display'variable make a tuple with the keyNames
+of table model in models.py. Then pass the class name in example: 'admin.site.register(Product, ClassName)' """
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'description', 'quantity')
+    """To add a filter in the admin dashboard in 'Product' table"""
+    list_filter = ['category']
+# Add models in the Admin Dashboard
+admin.site.register(Product, ProductAdmin)
--- inventory_management/dashboard/models.py
+++ inventory_management/dashboard/models.py
@@ -1,12 +1,63 @@
 from django.db import models
 
 # Create your models here.
+"""Product"""
+class Product(models.Model):
+    
+    name = models.CharField(max_length=100, null=True)
+    category = models.CharField(max_length=20)
+    description = models.TextField(max_length=200)
+    quantity = models.PositiveIntegerField(null=True)
 
-class Supplier(models.Model):
-    name = models.CharField(max_length=100)
-    contact_name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=20)
-
+    # to return in admin dashboard the name of the columns
     def __str__(self):
-        return self.name
+        return f'Name: {self.name}. Quantity: {self.quantity}'
+
+"""Client"""    
+class Client(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    telephone = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    # to return in admin dashboard the name of the columns
+    def __str__(self):
+        return f'Client Name: {self.name}. telephone: {self.telephone}. email: {self.email}'
+
+"""Supplier"""    
+class Supplier(models.Model):
+    category = models.CharField(max_length=20)
+    name = models.CharField(max_length=100, null=True)
+    telephone = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    # to return in admin dashboard the name of the columns
+    def __str__(self):
+        return f'Category: {self.category}. Client Name: {self.name}. telephone: {self.telephone}. email: {self.email}'
+
+
+"""purchase order"""    
+class PurchaseOrder(models.Model):
+    orderCode = models.CharField(max_length=20, null=True)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE, related_name='purchase_orders')
+    supplier = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    order_date = models.DateField()
+    
+    # to return in admin dashboard the name of the columns
+    def __str__(self):
+        return f'Order: {self.orderCode}. Product Name: {self.product}. Supplier: {self.supplier}. Qty: {self.quantity}. date: {self.order_date}'
+
+"""Client order"""    
+class ClientOrder(models.Model):
+    orderCode = models.CharField(max_length=20, null=True)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE, related_name='client_orders')
+    client = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    order_date = models.DateField()
+    
+    # to return in admin dashboard the name of the columns
+    def __str__(self):
+        return f'Order: {self.orderCode}. Product Name: {self.product}. Client: {self.client}. Qty: {self.quantity}. date: {self.order_date}'
+
+
+
