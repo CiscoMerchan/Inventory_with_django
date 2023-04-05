73f215497b5ce6f15724935e279819c783fa5afb
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-04-05 10:45:27 +1000

    Login and Logout

--- inventory_management/inventory_management/settings.py
+++ inventory_management/inventory_management/settings.py
@@ -138,3 +138,6 @@ STATIC_ROOT = (BASE_DIR/'asert/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Once the user login this is the url address where the user will be redirect
+LOGIN_REDIRECT_URL = 'dashboard-index'
