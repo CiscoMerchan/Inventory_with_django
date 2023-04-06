77850a7e6d7b296ffb12c3462a6acaa9e4ddd325
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-04-06 13:10:42 +1000

    Supplier Edit and Delete urls.py

--- inventory_management/dashboard/urls.py
+++ inventory_management/dashboard/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
     path('client/update/<int:pk>', views.client_update, name='dashboard-client-update'),
     path('client/delete/<int:pk>', views.client_delete, name='dashboard-client-delete'),
     path('supplier/', views.supplier, name='dashboard-supplier'),
+    path('supplier/update/<int:pk>', views.supplier_update, name='dashboard-supplier-update'),
+    path('supplier/delete/<int:pk>', views.supplier_delete, name='dashboard-supplier-delete'),
     
 ]
 # path('suppliers/', supplier_list, name='supplier_list'),
